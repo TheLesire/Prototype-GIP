46be072b9aad45a472b4004231104696b9fecdf1
TheLesire <Zakhery007@gmail.com>
2018-09-23 11:00:50 +0200

    Animations

---
+++ Prototype GIP/Assets/Anmator.cs
@@ -0,0 +1,42 @@
+﻿public float speed = 1.0f;
+public float jumpSpeed = 0.5f;
+public LayerMask groundLayer;
+
+private Animator marioAnimator;
+private Transform gCheck;
+private float scaleX = 1.0f;
+private float scaleY = 1.0f;
+
+void Start()
+{
+    marioAnimator = GetComponent();
+    gCheck = transform.FindChild("GCheck");
+
+}
+
+void FixedUpdate()
+{
+    float speed = 1.0f;
+    float mSpeed = Input.GetAxis("Horizontal");
+    marioAnimator.SetFloat("Speed", Mathf.Abs(mSpeed));
+    bool isTouched = Physics2D.OverlapPoint(gCheck.position, groundLayer);
+
+    if (Input.GetKey(KeyCode.Space))
+    {
+
+        if (isTouched)
+        {
+            rigidbody2D.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Force);
+            isTouched = false;
+        }
+    }
+    marioAnimator.SetBool("isTouched", isTouched);
+
+    if (mSpeed & gt; 0){
+        transform.localScale = new UnityEngine.Vector2(scaleX, scaleY);
+    }
+		else if (mSpeed & lt; 0){
+        transform.localScale = new UnityEngine.Vector2(-scaleX, scaleY);
+    }
+
+    this.rigidbody2D.velocity = new UnityEngine.Vector2(mSpeed * speed, rb = GetComponent<rigidbody2D>(velocity.y);
---
+++ Prototype GIP/Assets/Moving.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+    }
+}
