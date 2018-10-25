a9d6d9ad5d888d3f927a3fd5c1363512e786e5f1
TheLesire <Zakhery007@gmail.com>
2018-10-25 16:30:12 +0200

    CoinPickups/Counter

---
+++ Prototype GIP/Assets/Scripts/Coin_Script.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin_Script : MonoBehaviour {
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        ScoreScript.coinAmount += 1;
+        Destroy (gameObject);
+    }
+}
--- Prototype GIP/Assets/Scripts/PlayerController.cs
+++ Prototype GIP/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float maxSpeed = 7f;
+    [SerializeField] float maxSpeed = 7f; //visueel decleraren (ook in Unity)
     [SerializeField] float jumpPower = 330f;
     [SerializeField] Transform GroundTrigger;
     [SerializeField] float GroundTriggerRadius = 1f;
@@ -15,70 +15,70 @@ public class PlayerController : MonoBehaviour
 
 
 
-    Rigidbody2D rb;
-    SpriteRenderer sr;
+    Rigidbody2D rb; //Parameter Rigidbody2D is rd
+    SpriteRenderer sr; // parameter SpriteRenderer wordt sr
     float curSpeed = 0f;
     bool jump = false;
     bool isGrounded = false;
 
-
-    private void Awake()
+    
+    private void Awake() //rb en sr goed instellen
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
     }
 
-    private void Update()
+    private void Update() //deze wordt opgeroepen bij een nieuwe update van de scene
     {
-        curSpeed = Input.GetAxis("Horizontal") * maxSpeed;
+        curSpeed = Input.GetAxis("Horizontal") * maxSpeed; //De snelheid is gelijk aan de richting*de maximumsnelheid
 
-        ChangeDirection();
+        ChangeDirection(); //roept de functie ChangeDirection op
 
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded) //kijk of het personage kan en wil springen
         {
-            jump = true;
+            jump = true; //srping
         }
     }
 
 
     private void FixedUpdate()
     {
-        isGrounded = Physics2D.OverlapCircle(GroundTrigger.position, GroundTriggerRadius, GroundLayer);
-        Move();
-        Jump();
+        isGrounded = Physics2D.OverlapCircle(GroundTrigger.position, GroundTriggerRadius, GroundLayer); //vraagt zich af of het personage op de grond staat
+        Move(); //roept de functie bewegen op   
+        Jump(); //roept de functie springen op
     }
 
 
-    private void OnDrawGizmosSelected()
+    private void OnDrawGizmosSelected() //geeft de groundtrigger een kleur en cirkel
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawSphere(GroundTrigger.position, GroundTriggerRadius);
     }
-    private void Move()
+    private void Move() //De functie om te bewegen
     {
-        rb.velocity = new Vector2(curSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(curSpeed, rb.velocity.y); //De snelheid is gelijk aan de parameters tegenwoordigesnelheid en de snelheid op de y as
     }
 
 
-    void ChangeDirection()
+    void ChangeDirection() //De functie om het personage van kant te laten veranderen
     {
-        if( curSpeed > 0 && sr.flipX )
+        if( curSpeed > 0 && sr.flipX ) //Als de tegenwoordige snelheid kleiner is als 0 en het personage is nog niet omgedraaid
         {
-            sr.flipX = false;
+            sr.flipX = false; //Draai dan om
         }
-        else if( curSpeed < 0 && !sr.flipX )
+        else if( curSpeed < 0 && !sr.flipX) //Als de tegenwoordige snelheid kleiner is als 0 en het personage is al omgedraaid
         {
-            sr.flipX = true;
+            sr.flipX = true; //Draai dan terug om
         }
     }
 
 
-    void Jump()
+    void Jump() //De functie om te springen
     {
-        if(jump)
+        if(jump) //als je wilt jumpen
         {
-            jump = false;
-            rb.AddForce(Vector2.up * jumpPower);
+            jump = false; //Dan kan je niet nog is jumpen
+            rb.AddForce(Vector2.up * jumpPower); //versnel aan hand van de y functie * de jumpPower
         }
         
 
