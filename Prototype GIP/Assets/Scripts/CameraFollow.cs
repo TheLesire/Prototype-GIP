0bb26e1c49ab3693a52c3cb11022164c8af25904
TheLesire <Zakhery007@gmail.com>
2018-11-20 10:12:27 +0100

    Camera follow

---
+++ Prototype GIP/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform Player;
+    public float cameraDistance = 30.0f;
+    public float Hoogte;
+
+    private void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+    }
+
+    void FixedUpdate()
+    {
+        Height();
+        transform.position = new Vector3(Player.position.x, Hoogte, -10);
+    }
+
+    void Height()
+    {
+        if(Player.transform.position.y > 7)
+        {
+            Hoogte = 7;
+        }
+        else
+        {
+            Hoogte = 5;
+        }
+    }
+}
